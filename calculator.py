ae6a6aa73900af842414a33b10653be4a9f6e022
ganesh dasari <ganeshdasari2005@gmail.com>
2026-04-29 10:11:46 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,12 @@
+a = int(input("enter first operand: "))
+o = input("enter operator: ")
+b = int(input("enter second operand: "))
+
+if o == '+':
+  print(a + b)
+elif o == '-':
+  print(a - b)
+elif o == '*':
+  print(a * b)
+elif o == '/':
+  print(a / b)
